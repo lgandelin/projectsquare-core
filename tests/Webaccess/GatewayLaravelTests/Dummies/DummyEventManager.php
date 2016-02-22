a15b5f9264b9d9aca1cf21d9407d924a7a503e70
Louis Gandelin <lgandelin@web-access.fr>
2016-02-22 15:55:00 +0100

    Finished Behat first feature

--- tests/Webaccess/GatewayLaravelTests/Dummies/DummyEventManager.php
+++ tests/Webaccess/GatewayLaravelTests/Dummies/DummyEventManager.php
@@ -6,7 +6,20 @@ use Webaccess\GatewayLaravel\EventManager;
 
 class DummyEventManager implements EventManager
 {
+    public static $firedEvents;
+
+    public function __construct()
+    {
+        self::$firedEvents = [];
+    }
+
     public function fire($event)
     {
+        self::$firedEvents[]= $event;
+    }
+
+    public function getFiredEvents()
+    {
+        return self::$firedEvents;
     }
 }
