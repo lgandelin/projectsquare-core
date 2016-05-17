13234aad55232929635a7b295a11e6d32ab536a9
Louis Gandelin <lgandelin@web-access.fr>
2016-05-17 10:53:01 +0200

    Notifications are now deleted with the event associated

--- tests/Webaccess/ProjectSquareTests/Interactors/Events/DeleteEventInteractorTest.php
+++ tests/Webaccess/ProjectSquareTests/Interactors/Events/DeleteEventInteractorTest.php
@@ -4,7 +4,9 @@ use Webaccess\ProjectSquare\Context;
 use Webaccess\ProjectSquare\Entities\Event;
 use Webaccess\ProjectSquare\Events\Events\DeleteEventEvent;
 use Webaccess\ProjectSquare\Events\Events;
+use Webaccess\ProjectSquare\Interactors\Events\CreateEventInteractor;
 use Webaccess\ProjectSquare\Interactors\Events\DeleteEventInteractor;
+use Webaccess\ProjectSquare\Requests\Events\CreateEventRequest;
 use Webaccess\ProjectSquare\Requests\Events\DeleteEventRequest;
 use Webaccess\ProjectSquare\Responses\Events\DeleteEventResponse;
 use Webaccess\ProjectSquareTests\BaseTestCase;
@@ -14,7 +16,7 @@ class DeleteEventInteractorTest extends BaseTestCase
     public function __construct()
     {
         parent::__construct();
-        $this->interactor = new DeleteEventInteractor($this->eventRepository);
+        $this->interactor = new DeleteEventInteractor($this->eventRepository, $this->notificationRepository);
     }
 
     /**
@@ -26,20 +28,6 @@ class DeleteEventInteractorTest extends BaseTestCase
             'eventID' => 1,
         ]));
     }
-
-    /*
-     * @expectedException Exception
-     */
-    /*public function testDeleteEventWithoutPermission()
-    {
-        $user1 = $this->createSampleUser();
-        $user2 = $this->createSampleUser();
-        $event = $this->createSampleEvent($user1->id);
-        $this->interactor->execute(new DeleteEventRequest([
-            'eventID' => $event->id,
-            'requesterUserID' => $user2->id
-        ]));
-    }*/
 
     public function testDeleteEvent()
     {
@@ -63,5 +51,37 @@ class DeleteEventInteractorTest extends BaseTestCase
             Events::DELETE_EVENT,
             Mockery::type(DeleteEventEvent::class)
         );
+    }
+
+    public function testDeleteAnotherUserEvent()
+    {
+        $user = $this->createSampleUser();
+
+        $response = (new CreateEventInteractor(
+            $this->eventRepository,
+            $this->notificationRepository
+        ))->execute(new CreateEventRequest([
+            'name' => 'Sample event',
+            'startTime' => new \DateTime('2016-03-15 10:30:00'),
+            'endTime' => new \DateTime('2016-03-15 18:30:00'),
+            'userID' => $user->id,
+            'requesterUserID' => 2,
+        ]));
+
+        $response = $this->interactor->execute(new DeleteEventRequest([
+            'eventID' => $response->event->id,
+            'requesterUserID' => $user->id
+        ]));
+
+        //Check response
+        $this->assertInstanceOf(DeleteEventResponse::class, $response);
+        $this->assertInstanceOf(Event::class, $response->event);
+        $this->assertEquals($response->event->id, $response->event->id);
+
+        //Check deletion
+        $this->assertCount(0, $this->eventRepository->objects);
+
+        //Check that notifications are deleted
+        $this->assertEquals(0, count($this->notificationRepository->getNotifications($user->id)));
     }
 }
