9bf52e12b62e30f46edfe5d25101b400c9fd9a90
Louis Gandelin <lgandelin@web-access.fr>
2016-08-19 10:38:10 +0200

    Added GetTicketsTotalTimeInteractor class

---
+++ src/Webaccess/ProjectSquare/Responses/Progress/GetTicketsTotalTimeResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Webaccess\ProjectSquare\Responses\Progress;
+
+use Webaccess\ProjectSquare\Responses\Response;
+
+class GetTicketsTotalTimeResponse extends Response
+{
+    public $days;
+    public $hours;
+}
