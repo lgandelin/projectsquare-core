8f7adba49ec95dbc7d644834ab20232095b62a3d
Louis Gandelin <lgandelin@web-access.fr>
2017-03-15 10:05:02 +0100

    Updated Response class

--- src/Webaccess/ProjectSquare/Responses/Response.php
+++ src/Webaccess/ProjectSquare/Responses/Response.php
@@ -7,7 +7,7 @@ class Response
     public function __construct($params = array())
     {
         foreach ($params as $key => $value) {
-            if (property_exists($this, $key) && $value != null) {
+            if (property_exists($this, $key) && $value !== null) {
                 $this->$key = $value;
             }
         }
